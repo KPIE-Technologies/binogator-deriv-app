1858e75ef82f5de4e2a049a29f60fd93d89f6684
oluwaturheeb <oluwaturheeb@gmail.com>
2023-04-29 23:42:51 +0100

    Updates

--- account/js/poa-status-codes.js
+++ account/js/poa-status-codes.js
@@ -1,2 +1,109 @@
-/*! For license information please see poa-status-codes.js.LICENSE.txt */
-!function(e,_){"object"==typeof exports&&"object"==typeof module?module.exports=_():"function"==typeof define&&define.amd?define([],_):"object"==typeof exports?exports["@deriv/account"]=_():e["@deriv/account"]=_()}(self,(()=>(()=>{"use strict";var __webpack_modules__={"./Components/poa/status/status-codes/index.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{eval('__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _status_codes__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./status-codes */ "./Components/poa/status/status-codes/status-codes.ts");\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_status_codes__WEBPACK_IMPORTED_MODULE_0__.poa_status_codes);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9Db21wb25lbnRzL3BvYS9zdGF0dXMvc3RhdHVzLWNvZGVzL2luZGV4LnRzLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFFQSIsInNvdXJjZXMiOlsid2VicGFjazovL0BkZXJpdi9hY2NvdW50Ly4vQ29tcG9uZW50cy9wb2Evc3RhdHVzL3N0YXR1cy1jb2Rlcy9pbmRleC50cz85MjQzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IHBvYV9zdGF0dXNfY29kZXMgfSBmcm9tICcuL3N0YXR1cy1jb2Rlcyc7XG5cbmV4cG9ydCBkZWZhdWx0IHBvYV9zdGF0dXNfY29kZXM7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./Components/poa/status/status-codes/index.ts\n')},"./Components/poa/status/status-codes/status-codes.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"poa_status_codes\": () => (/* binding */ poa_status_codes)\n/* harmony export */ });\nvar poa_status_codes = {\n  none: 'none',\n  pending: 'pending',\n  rejected: 'rejected',\n  verified: 'verified',\n  expired: 'expired',\n  suspected: 'suspected'\n};//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9Db21wb25lbnRzL3BvYS9zdGF0dXMvc3RhdHVzLWNvZGVzL3N0YXR1cy1jb2Rlcy50cy5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBRUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL0BkZXJpdi9hY2NvdW50Ly4vQ29tcG9uZW50cy9wb2Evc3RhdHVzL3N0YXR1cy1jb2Rlcy9zdGF0dXMtY29kZXMudHM/NjJkNSJdLCJzb3VyY2VzQ29udGVudCI6WyJ0eXBlIFRQb2FTdGF0dXNDb2RlcyA9IFJlYWRvbmx5PFJlY29yZDwnbm9uZScgfCAncGVuZGluZycgfCAncmVqZWN0ZWQnIHwgJ3ZlcmlmaWVkJyB8ICdleHBpcmVkJyB8ICdzdXNwZWN0ZWQnLCBzdHJpbmc+PjtcblxuZXhwb3J0IGNvbnN0IHBvYV9zdGF0dXNfY29kZXM6IFRQb2FTdGF0dXNDb2RlcyA9IHtcbiAgICBub25lOiAnbm9uZScsXG4gICAgcGVuZGluZzogJ3BlbmRpbmcnLFxuICAgIHJlamVjdGVkOiAncmVqZWN0ZWQnLFxuICAgIHZlcmlmaWVkOiAndmVyaWZpZWQnLFxuICAgIGV4cGlyZWQ6ICdleHBpcmVkJyxcbiAgICBzdXNwZWN0ZWQ6ICdzdXNwZWN0ZWQnLFxufTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./Components/poa/status/status-codes/status-codes.ts\n")}},__webpack_module_cache__={};function __webpack_require__(e){var _=__webpack_module_cache__[e];if(void 0!==_)return _.exports;var t=__webpack_module_cache__[e]={exports:{}};return __webpack_modules__[e](t,t.exports,__webpack_require__),t.exports}__webpack_require__.d=(e,_)=>{for(var t in _)__webpack_require__.o(_,t)&&!__webpack_require__.o(e,t)&&Object.defineProperty(e,t,{enumerable:!0,get:_[t]})},__webpack_require__.o=(e,_)=>Object.prototype.hasOwnProperty.call(e,_),__webpack_require__.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})};var __webpack_exports__=__webpack_require__("./Components/poa/status/status-codes/index.ts");return __webpack_exports__=__webpack_exports__.default,__webpack_exports__})()));
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["@deriv/account"] = factory();
+	else
+		root["@deriv/account"] = factory();
+})(self, () => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./Components/poa/status/status-codes/index.ts":
+/*!*****************************************************!*\
+  !*** ./Components/poa/status/status-codes/index.ts ***!
+  \*****************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _status_codes__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./status-codes */ \"./Components/poa/status/status-codes/status-codes.ts\");\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_status_codes__WEBPACK_IMPORTED_MODULE_0__.poa_status_codes);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9Db21wb25lbnRzL3BvYS9zdGF0dXMvc3RhdHVzLWNvZGVzL2luZGV4LnRzLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFFQSIsInNvdXJjZXMiOlsid2VicGFjazovL0BkZXJpdi9hY2NvdW50Ly4vQ29tcG9uZW50cy9wb2Evc3RhdHVzL3N0YXR1cy1jb2Rlcy9pbmRleC50cz85MjQzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IHBvYV9zdGF0dXNfY29kZXMgfSBmcm9tICcuL3N0YXR1cy1jb2Rlcyc7XG5cbmV4cG9ydCBkZWZhdWx0IHBvYV9zdGF0dXNfY29kZXM7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./Components/poa/status/status-codes/index.ts\n");
+
+/***/ }),
+
+/***/ "./Components/poa/status/status-codes/status-codes.ts":
+/*!************************************************************!*\
+  !*** ./Components/poa/status/status-codes/status-codes.ts ***!
+  \************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"poa_status_codes\": () => (/* binding */ poa_status_codes)\n/* harmony export */ });\nvar poa_status_codes = {\n  none: 'none',\n  pending: 'pending',\n  rejected: 'rejected',\n  verified: 'verified',\n  expired: 'expired',\n  suspected: 'suspected'\n};//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9Db21wb25lbnRzL3BvYS9zdGF0dXMvc3RhdHVzLWNvZGVzL3N0YXR1cy1jb2Rlcy50cy5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBRUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL0BkZXJpdi9hY2NvdW50Ly4vQ29tcG9uZW50cy9wb2Evc3RhdHVzL3N0YXR1cy1jb2Rlcy9zdGF0dXMtY29kZXMudHM/NjJkNSJdLCJzb3VyY2VzQ29udGVudCI6WyJ0eXBlIFRQb2FTdGF0dXNDb2RlcyA9IFJlYWRvbmx5PFJlY29yZDwnbm9uZScgfCAncGVuZGluZycgfCAncmVqZWN0ZWQnIHwgJ3ZlcmlmaWVkJyB8ICdleHBpcmVkJyB8ICdzdXNwZWN0ZWQnLCBzdHJpbmc+PjtcblxuZXhwb3J0IGNvbnN0IHBvYV9zdGF0dXNfY29kZXM6IFRQb2FTdGF0dXNDb2RlcyA9IHtcbiAgICBub25lOiAnbm9uZScsXG4gICAgcGVuZGluZzogJ3BlbmRpbmcnLFxuICAgIHJlamVjdGVkOiAncmVqZWN0ZWQnLFxuICAgIHZlcmlmaWVkOiAndmVyaWZpZWQnLFxuICAgIGV4cGlyZWQ6ICdleHBpcmVkJyxcbiAgICBzdXNwZWN0ZWQ6ICdzdXNwZWN0ZWQnLFxufTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./Components/poa/status/status-codes/status-codes.ts\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./Components/poa/status/status-codes/index.ts");
+/******/ 	__webpack_exports__ = __webpack_exports__["default"];
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
